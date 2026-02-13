cbdb3b3d2058250b23bb290e8b1642375d02308b
Poseidon2579 <poseidon2579@gmail.com>
2026-02-13 16:50:33 -0400

    fix: correct syntax error in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /* config options here */
-    /* Force rebuild: 2026-02-12T22:20:00 */
     typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
         ignoreBuildErrors: true,
     },
     eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
         ignoreDuringBuilds: true,
     },
-    // Performance Optimizations
     compress: true,
-    swcMinify: true,
-    view: true,
     images: {
         formats: ['image/avif', 'image/webp'],
         minimumCacheTTL: 60,
     },
-    compiler: {
-        removeConsole: process.env.NODE_ENV === "production" ? { exclude: ['error'] } : false,
-    },
+};
 
-    export default nextConfig;
+export default nextConfig;
